4d83b35efa496241ac98ec8c13f271c4293edbf5
jinyujiao <jinyujiao@gmail.com>
2026-07-13 08:02:26 +0800

    fix: lock phone orientation before activity launch

--- app/src/main/java/com/yura/app/util/DeviceOrientationPolicy.kt
+++ app/src/main/java/com/yura/app/util/DeviceOrientationPolicy.kt
@@ -5,9 +5,12 @@ import android.content.pm.ActivityInfo
 import com.yura.app.R
 
 fun Activity.applyDeviceOrientationPolicy() {
-    requestedOrientation = if (resources.getBoolean(R.bool.is_tablet)) {
+    val targetOrientation = if (resources.getBoolean(R.bool.is_tablet)) {
         ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
     } else {
         ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+    if (requestedOrientation != targetOrientation) {
+        requestedOrientation = targetOrientation
     }
 }
